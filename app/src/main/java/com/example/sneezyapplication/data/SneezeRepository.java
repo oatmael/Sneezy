c66169aecf25d97f189aa350596ff970bdb7e2c7
oatmael <jaronharrison1605@gmail.com>
2020-06-05 01:41:31 +0930

    Clean up methods, all users fix
    Fixed a small bug where the query for getting records that are not the users was incorrect.
    
    Cleaned up methods and implemented a monthly sneezes collection for current month.
    
    To-do:
    current week,
    scrolling to past weeks/months

--- app/src/main/java/com/example/sneezyapplication/data/SneezeRepository.java
+++ app/src/main/java/com/example/sneezyapplication/data/SneezeRepository.java
@@ -1,24 +1,23 @@
 package com.example.sneezyapplication.data;
 
-import android.support.annotation.NonNull;
-import android.util.Log;
-
 import com.example.sneezyapplication.MainActivity;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.mongodb.stitch.android.services.mongodb.remote.RemoteFindIterable;
 
 import org.bson.BsonDocument;
 import org.bson.BsonString;
-import org.bson.Document;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class SneezeRepository {
 
     private List<SneezeItem> allSneezeItems;
     private List<SneezeItem> allUserSneezeItems;
+    private List<SneezeItem> monthlyUserSneezeItems;
 
     public List<SneezeItem> getAllSneezeItems() {
         return allSneezeItems;
@@ -26,26 +25,30 @@ public class SneezeRepository {
     public List<SneezeItem> getAllUserSneezeItems() {
         return allUserSneezeItems;
     }
+    public List<SneezeItem> getMonthlyUserSneezeItems() { return monthlyUserSneezeItems; }
 
     public SneezeRepository(){
         allSneezeItems = new ArrayList<>();
         allUserSneezeItems = new ArrayList<>();
+        monthlyUserSneezeItems = new ArrayList<>();
     }
 
     public void updateRecords(MainActivity activity) {
 
+        updateAllSneezes(activity);
+        updateUserSneezes(activity);
+        updateUserMonthlySneezes();
+
+    }
+
+    private void updateAllSneezes(MainActivity activity){
         BsonDocument allRecordsFilter = new BsonDocument()
-                .append("$not", new BsonDocument()
-                        .append(SneezeItem.Fields.OWNER_ID, new BsonString(activity.getUserID())));
+                .append(SneezeItem.Fields.OWNER_ID, new BsonDocument()
+                        .append("$ne", new BsonString(activity.getUserID())));
 
         // Potentially omit owner_id from result for more security?
         BsonDocument allRecordsProjectionFilter = new BsonDocument()
                 .append(SneezeItem.Fields.OWNER_ID, new BsonString("0"));
-
-        BsonDocument userRecordsFilter = new BsonDocument()
-                .append(SneezeItem.Fields.OWNER_ID, new BsonString(activity.getUserID()));
-
-
 
         RemoteFindIterable allRecordsResults = activity.getItems()
                 .find(allRecordsFilter)
@@ -59,6 +62,11 @@ public class SneezeRepository {
                 // Do something on update?
             }
         });
+    }
+
+    private void updateUserSneezes(MainActivity activity){
+        BsonDocument userRecordsFilter = new BsonDocument()
+                .append(SneezeItem.Fields.OWNER_ID, new BsonString(activity.getUserID()));
 
         RemoteFindIterable userRecordsResults = activity.getItems()
                 .find(userRecordsFilter)
@@ -67,10 +75,25 @@ public class SneezeRepository {
 
         Task<List<SneezeItem>> userRecordsTask = userRecordsResults.into(this.allUserSneezeItems);
         userRecordsTask.addOnCompleteListener(task -> {
-           if (task.isSuccessful()){
+            if (task.isSuccessful()){
                 // Do something on update?
-           }
+            }
         });
+    }
+
+    private void updateUserMonthlySneezes(){
+        DateFormat monthFormat = new SimpleDateFormat("MMM");
+        Date date = new Date();
+        String month = monthFormat.format(date);
+
+        for (SneezeItem s : allUserSneezeItems){
+            if (s.getDate().contains(month)){
+                monthlyUserSneezeItems.add(s);
+            }
+        }
+    }
+    private void updateUserWeeklySneezes(){
+
     }
 
 
