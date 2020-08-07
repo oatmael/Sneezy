f6be8629e17d7b8105731ca8c7cba0500d1629f4
SbrowneA <samuel.browne.mx@gmail.com>
2020-08-07 14:43:32 +0930

    Revert "Forecast Retreival added (Not hooked up to front-end))"
    This reverts commit f8b002f66435836e5e0f82fbd8ef03fa00e368b7.

--- app/src/main/java/com/app/sneezyapplication/SharedPref.java
+++ app/src/main/java/com/app/sneezyapplication/SharedPref.java
@@ -2,14 +2,13 @@ package com.app.sneezyapplication;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.util.Log;
-import android.widget.Toast;
 
 public class SharedPref {
 
-    SharedPreferences colourModePref;//may need to refactor name to something like sharedPrefs
+    SharedPreferences colourModePref;
     public SharedPref(Context context) {
         colourModePref = context.getSharedPreferences("filename", Context.MODE_PRIVATE);
+
     }
 
     public void setNightModeState(Boolean state) {
@@ -22,27 +21,5 @@ public class SharedPref {
         Boolean state = colourModePref.getBoolean("NightMode", false);
         return state;
     }
-
-
-
-//    public static final String SHARED_PREFS = "sharedPrefs";
-    public static final String LOCATION_INDEX_KEY= "locationIndex";
-
-
-    public void saveLocationPreference(int locationIndex){
-        //TODO before calling* unless can be called from popup save btn
-        //check if locationIndex == forecastObj.getSelectedCityNo()
-        //not true update locationIndex
-        SharedPreferences.Editor editor = colourModePref.edit();
-        editor.putInt(LOCATION_INDEX_KEY, locationIndex);
-        editor.commit();
-        Log.d("SharedPref","locationIndex " + locationIndex + " successfully saved to shared preferences");
-    }
-
-    public int loadLocationPreference(){
-        int locationIndex = colourModePref.getInt(LOCATION_INDEX_KEY, -1);
-        return locationIndex;
-    }
-
 
 }
