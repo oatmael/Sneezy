58122d2a1389ce149f74848408720f1c65593557
SbrowneA <samuel.browne.mx@gmail.com>
2020-08-14 13:53:53 +0930

    Added google maps dependencies to gradle
    Signed-off-by: SbrowneA <samuel.browne.mx@gmail.com>

--- app/src/main/java/com/app/sneezyapplication/MapsFragment.java
+++ app/src/main/java/com/app/sneezyapplication/MapsFragment.java
@@ -1,17 +1,44 @@
 package com.app.sneezyapplication;
 
 import android.os.Bundle;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MapsFragment extends Fragment {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.CameraUpdateFactory;
+
+public class MapsFragment extends Fragment{
+
+    private GoogleMap mMap;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_maps, container, false);
     }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        MapView mapView = getView().findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+    }
+
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//        //TODO call get map async
+//        mMap = googleMap;
+//
+//    }
 }
