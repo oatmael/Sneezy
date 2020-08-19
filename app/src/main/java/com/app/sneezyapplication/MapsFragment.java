82106b43f160836b88d68e541772189c30576f1c
SbrowneA <samuel.browne.mx@gmail.com>
2020-08-20 00:35:46 +0930

    New: Button to center map at user location
    Signed-off-by: SbrowneA <samuel.browne.mx@gmail.com>

--- app/src/main/java/com/app/sneezyapplication/MapsFragment.java
+++ app/src/main/java/com/app/sneezyapplication/MapsFragment.java
@@ -1,9 +1,9 @@
 package com.app.sneezyapplication;
 
-import android.graphics.Color;
-import android.hardware.camera2.TotalCaptureResult;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,38 +11,41 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.common.util.IOUtils;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.model.Tile;
 import com.google.android.gms.maps.model.TileOverlay;
 import com.google.android.gms.maps.model.TileOverlayOptions;
-import com.google.maps.android.heatmaps.Gradient;
 import com.google.maps.android.heatmaps.HeatmapTileProvider;
 
-import java.lang.reflect.Array;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
 
-public class MapsFragment extends Fragment implements OnMapReadyCallback {
+public class MapsFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener,
+        GoogleMap.OnMyLocationClickListener {
 
     private MapView mMapView;
     private HeatmapTileProvider mProvider;
-    TileOverlay mOverlay;
+    private TileOverlay mOverlay;
+    private GoogleMap googleMap;
     private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
-    private static final String CLASS_TAG ="MapsFragment";
+    private static final String CLASS_TAG = "MapsFragment";
+    private static final LatLng DEFAULT_LOCATION = new LatLng(-30, 133);
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -53,9 +56,7 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // *** IMPORTANT ***
-        // MapView requires that the Bundle you pass contain _ONLY_ MapView SDK
-        // objects or sub-Bundles.
+        // *** MapView requires that the Bundle you pass contain _ONLY_ MapView SDK objects or sub-Bundles. ***
         Bundle mapViewBundle = null;
         if (savedInstanceState != null) {
             mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
@@ -64,7 +65,13 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
         mMapView.onCreate(mapViewBundle);
 
         mMapView.getMapAsync(this);
-    }
+
+//        FloatingActionButton myLocationBtn = getView()).findViewById(R.id.btn_my_location);
+//        myLocationBtn.setOnClickListener(v ->
+//                        onMyLocationButtonClick()
+//        );
+    }//onViewCreated END
+
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
@@ -78,76 +85,90 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        //dummy data**
+    public void onMapReady(GoogleMap gMap) {
+        googleMap = gMap;
+
+        //============== dummy data ==============
         ArrayList<LatLng> sneezeLocations = new ArrayList<>();
         double[] lats = {-37.1886, -37.8361, -38.4034, -38.7597, -36.9672};
         double[] lngs = {145.708, 144.845, 144.192, 143.67, 141.083};
 
         //make a list of sneeze locations with lat & lng values
         for (int i = 0; i < lats.length; i++) {
-            sneezeLocations.add(new LatLng(lats[i],lngs[i]));
+            sneezeLocations.add(new LatLng(lats[i], lngs[i]));
         }
 
+
+        //============== dummy data ==============
+        //TODO Try-Catch for no data retrieved
+        //TODO check theme to apply dark styled-map
         //Create a heat map tile provider and overlay
-        try {
-            mProvider = new HeatmapTileProvider.Builder().data(sneezeLocations).build();
-            mOverlay = googleMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
-        }
-        catch (Exception ex){
-            Log.e(CLASS_TAG," Heat map could not be generated:\n\t"+ex);
-        }
-
-        //TODO show user location
-        //TODO set starting location at user location
-
-        //Other customisations..
-        /*
-        // Create Custom Gradient
-        int[] colors = {
-                Color.rgb(102, 225, 0), // green
-                Color.rgb(255, 0, 0)    // red
-        };
-
-        float[] startPoints = {
-                0.2f, 1f
-        };
-
-        Gradient gradient = new Gradient(colors, startPoints);
-
-// Create the tile provider.
-        mProvider = new HeatmapTileProvider.Builder()
-                .data(list)
-                .gradient(gradient)
-                .build();
-
-        // Add the tile overlay to the map.
+        mProvider = new HeatmapTileProvider.Builder().data(sneezeLocations).build();
         mOverlay = googleMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+        //TODO check if location services are enabled before MyLocation permission check
 
-        //To change the opacity of an existing heatmap:
-        mProvider.setOpacity(0.7);
-        mOverlay.clearTileCache();
-    }//onMapReady END
+        //Permission check to enable MyLocation and MyLocationButton
+        if (MyLocationPermissionCheck()) {
+            //Initialize MyLocation button
+            Toast.makeText(getContext(), "myLocationEnabled",Toast.LENGTH_LONG).show();
+            googleMap.setOnMyLocationButtonClickListener(this);
+            googleMap.setOnMyLocationClickListener(this);
+            //hide default MyLocation button
+//            googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+            //set starting location to be users location
+//            onMyLocationButtonClick();
+        }
+        else{
+            //set starting location to default location
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(DEFAULT_LOCATION));
 
-    private void addHeatMap(GoogleMap googleMap) {
-        List<LatLng> list = null;
-        double[] lats = {-37.1886, -37.8361, -38.4034, -38.7597, -36.9672};
-        double[] lngs = {145.708, 144.845, 144.192, 143.67, 141.083};
-        //make a list of locations with lat & lng values
-        for (int i = 0; i < lats.length; i++) {
-            LatLng location = new LatLng(lats[i], lngs[i]);
-            list.add(location);
+            /*
+            //set MyLocationButton drawable to disabled
+            FloatingActionButton myLocationBtn = getView().findViewById(R.id.btn_my_location);
+            Drawable myLocationDisabled = getResources().getDrawable(R.drawable.ic_my_location_disabled);
+            myLocationBtn.setImageDrawable(myLocationDisabled);
+
+        //Set location services disabled
+//        Drawable locationDisabled = getResources().getDrawable(R.drawable.ic_my_location_disabled);//<<<< Location services off drawable
+//        myLocationBtn.setImageDrawable(locationDisabled);
+        int[][] states = new int[][] {
+                new int[] { android.R.attr.state_enabled}, // enabled
+                new int[] {-android.R.attr.state_enabled}, // disabled
+        };
+        int[] colors = new int[] {
+                R.color.google_blue,
+                R.color.darkButtonMain
+        };
+        ColorStateList tintList = new ColorStateList(states, colors);
+        myLocationBtn.setBackgroundTintList(tintList);
+        */
+
+            Toast.makeText(getContext(), "myLocationDisabled",Toast.LENGTH_LONG).show();
         }
 
-        // Create a heat map tile provider, passing it the latlngs of the police stations.
-        HeatmapTileProvider mProvider = new HeatmapTileProvider.Builder().data(list).build();
-        // Add a tile overlay to the map, using the heat map tile provider.
-        Object mOverlay = googleMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
-*/
     }//onMapReady END
+
+    private void changeTest(){
+
+    }
+//    private void addHeatMap(GoogleMap googleMap) { }
 
     //TODO add function to chose between day/week/month sneeze locations
-    //TODO ADD floating action button to center map at location
+
+    private boolean MyLocationPermissionCheck(){
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            //Permission not granted
+            return false;
+        }
+        //Permission granted
+        //enable MyLocation marker
+        googleMap.setMyLocationEnabled(true);
+        return true;
+
+    }
+    //OnMapReadyCallback METHODS
     @Override
     public void onResume() {
         super.onResume();
@@ -182,5 +203,23 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
         super.onLowMemory();
         mMapView.onLowMemory();
     }
+    //MyLocation METHODS
+    @Override
+    public boolean onMyLocationButtonClick() {
+        return false;
+    }
 
-}
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        /*
+//        Toast.makeText(getContext(),"MyLocationBtn executed",Toast.LENGTH_LONG).show();
+
+        if(location != null) { // Check to ensure coordinates aren't null, probably a better way of doing this...
+            googleMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+            googleMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        }
+
+         */
+    }
+
+}//MapsFragment Class END
