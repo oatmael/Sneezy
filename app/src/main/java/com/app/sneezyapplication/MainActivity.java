0fadba7f08e00aeee43b470fcc5364e6ad7b11ad
lost0000 <47601530+lost0000@users.noreply.github.com>
2020-08-25 19:30:03 +0930

    Debug Code/Strings Removed
    - Report email changed to official email
    - DPI Strings removed
    - Change Log Strings removed

--- app/src/main/java/com/app/sneezyapplication/MainActivity.java
+++ app/src/main/java/com/app/sneezyapplication/MainActivity.java
@@ -310,7 +310,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     protected void sendEmail() {
         Log.i("Send email", "");
-        String[] TO = {"tommy_b55@hotmail.com"};
+        String[] TO = {"sneezyapp@gmail.com"};
         /*        String[] CC = {"xyz@gmail.com"};*/
         Intent emailIntent = new Intent(Intent.ACTION_SEND);
         emailIntent.setData(Uri.parse("mailto:"));
@@ -320,7 +320,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
         /*        emailIntent.putExtra(Intent.EXTRA_CC, CC);*/
         emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Bug Report: ");
-        emailIntent.putExtra(Intent.EXTRA_TEXT, "Bug Report: Write your description of the bug, as well as the steps to make the bug occur. Screenshots help alot. Thanksyou.");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Bug Report: Write your description of the bug, as well as the steps to make the bug occur. Screenshots help a lot. Thank you.");
 
         try {
             startActivity(Intent.createChooser(emailIntent, "Send mail..."));
