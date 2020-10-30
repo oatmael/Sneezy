f0d26bfbd4468d04a9e20ae09c9b849d14565c08
oatmael <jaronharrison1605@gmail.com>
2020-10-30 15:23:08 +1030

    Fixes for new repo

--- app/src/main/java/com/app/sneezyapplication/data/GraphData.java
+++ app/src/main/java/com/app/sneezyapplication/data/GraphData.java
@@ -29,7 +29,7 @@ public class GraphData {
     }
 
     public ListViewItem getListViewGraphData(Date _date){
-        return getListViewGraphableData(repo.getSneezeItems(_date, SneezeRepository.Scope.USER, false));
+        return getListViewGraphableData(repo.getSneezeItems(_date, SneezeRepository.Scope.USER));
 
     }
 
